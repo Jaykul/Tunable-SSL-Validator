6ea737a5992a4cb606f5d5b2a4ba168437fb243f
unknown <MJenkins@RVV52706A.rva.local>
2015-07-27 13:01:33 -0400

    fixed Invoke-WebRequest and Invoke-RestMethod to handle -SessionVariable scope corectly

--- TunableSSLValidator.psm1
+++ TunableSSLValidator.psm1
@@ -294,7 +294,7 @@ function Invoke-WebRequest {
             $steppablePipeline.End()
             
             # If SessionVariable was specified, we need to set the it to the parents scope (otherwise the scope is limited to inside this function)
-            if ( $PSBoundParameters.ContainsKey("SessionVariable") ) { Set-Variable -Name "$SessionVariable" -Value $(Get-Variable -Name $SessionVariable -ValueOnly) -Scope 1 };
+            if ( $PSBoundParameters.ContainsKey("SessionVariable") ) { Set-Variable -Name "$SessionVariable" -Value $(Get-Variable -Name $SessionVariable -ValueOnly) -Scope 2 };
         } catch {
             throw
         }
@@ -423,7 +423,7 @@ function Invoke-RestMethod {
             $steppablePipeline.End()
             
             # If SessionVariable was specified, we need to set the it to the parents scope (otherwise the scope is limited to inside this function)
-            if ( $PSBoundParameters.ContainsKey("SessionVariable") ) { Set-Variable -Name "$SessionVariable" -Value $(Get-Variable -Name $SessionVariable -ValueOnly) -Scope 1 };
+            if ( $PSBoundParameters.ContainsKey("SessionVariable") ) { Set-Variable -Name "$SessionVariable" -Value $(Get-Variable -Name $SessionVariable -ValueOnly) -Scope 2 };
         } catch {
             throw
         }
